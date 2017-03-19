f62076c47ad019dbffe0bc1e9c45f9d524fbe2f1
galakt <gala-vadim@narod.ru>
2017-03-19 18:44:18 +0300

    Add task 1, task 3

---
+++ 2017/warmup/RCC_1/RCC_1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCC_1
+{
+    class Program
+    {
+        private static StringBuilder _sb = new StringBuilder();
+        static void Main(string[] args)
+        {
+            var maxCount = int.Parse(Console.ReadLine());
+            var tempPowers = Console.ReadLine().Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            var powers = new int[maxCount];
+            var summ = 0;
+
+            for (int i = 0; i < tempPowers.Length; i++)
+            {
+                var converted = int.Parse(tempPowers[i]);
+                powers[i] = converted;
+                summ += converted;
+            }
+
+            for (int i = 0; i < powers.Length; i++)
+            {
+                var current = powers[i];
+                if (summ - current == current)
+                {
+                    PrintPowers(powers, i);
+                    break;
+                }
+            }
+        }
+
+        private static void PrintPowers(int[] a, int lastIndex)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (i != lastIndex)
+                {
+                    _sb.Append(a[i]);
+                    _sb.Append(" ");
+                }
+            }
+
+            _sb.Append(a[lastIndex]);
+
+            Console.WriteLine(_sb.ToString());
+        }
+    }
+}
---
+++ 2017/warmup/RCC_3/RCC_3/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+
+namespace RCC_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // g, r и b 
+            var tempCounts = Console.ReadLine().Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+            var greenModels = new IntExt[int.Parse(tempCounts[0])];
+            var redModels = new IntExt[int.Parse(tempCounts[1])];
+            var blueModels = new IntExt[int.Parse(tempCounts[2])];
+
+            PopulateArrays(greenModels, Console.ReadLine());
+            PopulateArrays(redModels, Console.ReadLine());
+            PopulateArrays(blueModels, Console.ReadLine());
+
+            Array.Sort(redModels);
+
+            var counter = 0;
+            var lastRedItemCounter = 0;
+
+            for (int r = 0; r < redModels.Length; r++)
+            {
+                var currentR = redModels[r];
+
+                if (r > 0)
+                {
+                    if (redModels[r].Value == redModels[r - 1].Value)
+                    {
+                        counter += lastRedItemCounter;
+                        continue;
+                    }
+                    else
+                    {
+                        lastRedItemCounter = 0;
+                    }
+                }
+
+                for (int g = 0; g < greenModels.Length; g++)
+                {
+                    var currentG = greenModels[g];
+                    if (currentR.Value == currentG.Value)
+                    {
+                        continue;
+                    }
+
+                    if (currentR.FirstDigit != currentG.LastDigit)
+                    {
+                        continue;
+                    }
+
+                    for (int b = 0; b < blueModels.Length; b++)
+                    {
+                        var currentB = blueModels[b];
+                        if (currentB.Value == currentR.Value
+                            || currentB.Value == currentG.Value)
+                        {
+                            continue;
+                        }
+
+                        if (currentR.LastDigit != currentB.FirstDigit)
+                        {
+                            continue;
+                        }
+
+                        lastRedItemCounter++;
+                    }
+                }
+
+                counter += lastRedItemCounter;
+            }
+
+            Console.WriteLine(counter);
+        }
+
+        private static void PopulateArrays(IntExt[] a, string str)
+        {
+            var temp = str.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < temp.Length; i++)
+            {
+                a[i] = ConvertToIntExt(temp[i]);
+            }
+        }
+
+        private static IntExt ConvertToIntExt(string s)
+        {
+            var Value = int.Parse(s);
+            var firstDigit = 0;
+            var lastDigit = 0;
+
+            if (Value < 10)
+            {
+                return new IntExt(Value, Value, Value);
+            }
+            else if (Value < 100)
+            {
+                firstDigit = Value / 10;
+            }
+            else if (Value < 1000)
+                firstDigit = Value / 100;
+            else if (Value < 10000)
+                firstDigit = Value / 1000;
+            else if (Value < 100000)
+                firstDigit = Value / 10000;
+            else if (Value < 1000000)
+                firstDigit = Value / 100000;
+            else if (Value < 10000000)
+                firstDigit = Value / 1000000;
+            else if (Value < 100000000)
+                firstDigit = Value / 10000000;
+            else if (Value < 1000000000)
+                firstDigit = Value / 100000000;
+            else
+                firstDigit = Value / 1000000000;
+
+            lastDigit = Value % 10;
+
+            return new IntExt(Value, firstDigit, lastDigit);
+        }
+    }
+
+    public struct IntExt : IComparable<IntExt>
+    {
+        private int _value;
+
+        private int _firstDigit;
+
+        private int _lastDigit;
+
+        public IntExt(int value, int firstDigit, int lastDigit)
+        {
+            _value = value;
+            _lastDigit = lastDigit;
+            _firstDigit = firstDigit;
+        }
+
+        public int Value { get { return _value; }}
+
+        public int FirstDigit { get { return _firstDigit; } }
+
+        public int LastDigit { get { return _lastDigit; } }
+
+        public int CompareTo(IntExt other)
+        {
+            if (_value < other.Value) return -1;
+            if (_value > other.Value) return 1;
+            return 0;
+        }
+    }
+}
